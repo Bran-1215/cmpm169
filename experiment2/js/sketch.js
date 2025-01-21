8383b8e5fb47baa75b7fac8fad9a7f64771f4425
Bran-1215 <151097253+Bran-1215@users.noreply.github.com>
2025-01-20 20:37:32 -0800

    imitation test 2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,79 +1,84 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// M_6_1_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+/**
+ * distribute nodes on the display by letting them repel each other
+ *
+ * KEYS
+ * r             : reset positions
+ * s             : save png
+ */
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+'use strict';
 
-// Globals
-let myInstance;
-let canvasContainer;
-var centerHorz, centerVert;
+var sketch = function(p) {
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
+  // An array with nodes
+  var nodes = [];
+
+  var nodeCount = 200;
+
+  p.setup = function() {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.noStroke();
+
+    // Create nodes
+    createNodes();
+  };
+
+  p.draw = function() {
+    p.fill(255, 20);
+    p.rect(0, 0, p.width, p.height);
+
+    p.fill(0);
+    for (var i = 0; i < nodes.length; i++) {
+      // Let all nodes repel each other
+      nodes[i].attractNodes(nodes);
+      // Apply velocity vector and update position
+      nodes[i].update();
+      // Draw node
+      p.ellipse(nodes[i].x, nodes[i].y, 10, 10);
     }
+  };
 
-    myMethod() {
-        // code to run when method is called
+  p.keyPressed = function() {
+    if (p.key == 's' || p.key == 'S') p.saveCanvas(gd.timestamp(), 'png');
+    if (p.key == 'r' || p.key == 'R') {
+      p.background(255);
+      createNodes();
     }
-}
+  };
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
+  function createNodes() {
+    nodes = [];
+    for (var i = 0; i < nodeCount; i++) {
+      nodes.push(new Node(
+        p.width / 2 + p.random(-1, 1),
+        p.height / 2 + p.random(-1, 1),
+        5,
+        p.width - 5,
+        5,
+        p.height - 5
+      ));
+    }
+  }
 
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  // resize canvas is the page is resized
+};
 
-  // create an instance of the class
-  myInstance = new MyClass("VALUE1", "VALUE2");
-
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
-}
-
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
-
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
-  noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
-
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+var myp5 = new p5(sketch);
